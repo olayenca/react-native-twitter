4e9892045c9fda35cafd5b434addb7c8aa247da6
olayenca <olayinkaotuniyi@yahoo.co.uk>
2020-03-05 21:20:46 +0000

    prettier

--- src/oauth/auth.js
+++ src/oauth/auth.js
@@ -1,15 +1,13 @@
 import inAppBrowser from "react-native-inappbrowser-reborn";
-
-import {URLSearchParams } from 'whatwg-url';
-
+import { URLSearchParams } from 'whatwg-url';
 import request from './request';
-import {query} from '../util';
+import { query } from '../util';
 
 function getRequestToken(tokens, callbackUrl, accessType) {
   const method = 'POST';
   const url = 'https://api.twitter.com/oauth/request_token';
-  const body = accessType ? {x_auth_access_type: accessType} : {};
-  return request(tokens, url, {method, body}, {oauth_callback: callbackUrl})
+  const body = accessType ? { x_auth_access_type: accessType } : {};
+  return request(tokens, url, { method, body }, { oauth_callback: callbackUrl })
     .then(response => response.text())
     .then((text) => {
       const params = new URLSearchParams(text);
@@ -21,16 +19,16 @@ function getRequestToken(tokens, callbackUrl, accessType) {
 }
 
 function getAccessToken(
-  {consumerKey, consumerSecret, requestToken, requestTokenSecret},
+  { consumerKey, consumerSecret, requestToken, requestTokenSecret },
   oauthVerifier,
 ) {
   const method = 'POST';
   const url = 'https://api.twitter.com/oauth/access_token';
   return request(
-    {consumerKey, consumerSecret, oauthToken: requestToken, oauthTokenSecret: requestTokenSecret},
+    { consumerKey, consumerSecret, oauthToken: requestToken, oauthTokenSecret: requestTokenSecret },
     url,
-    {method},
-    {oauth_verifier: oauthVerifier},
+    { method },
+    { oauth_verifier: oauthVerifier },
   )
     .then(response => response.text())
     .then((text) => {
@@ -47,30 +45,29 @@ function getAccessToken(
 export default async function auth(
   tokens,
   callbackUrl,
-  {accessType, forSignIn = false, forceLogin = false, screenName = ''} = {},
+  { accessType, forSignIn = false, forceLogin = false, screenName = '' } = {},
 ) {
   const usePin = typeof callbackUrl.then === 'function';
-  const {requestToken, requestTokenSecret} = await getRequestToken(
+  const { requestToken, requestTokenSecret } = await getRequestToken(
     tokens,
     usePin ? 'oob' : callbackUrl,
     accessType,
   );
-  if(await inAppBrowser.isAvailable()){
- const result=   await inAppBrowser.openAuth(`https://api.twitter.com/oauth/${forSignIn ? 'authenticate' : 'authorize'}?${
-    query({oauth_token: requestToken, force_login: forceLogin, screen_name: screenName})
-  }`);
+  if (await inAppBrowser.isAvailable()) {
+    const result = await inAppBrowser.openAuth(`https://api.twitter.com/oauth/${forSignIn ? 'authenticate' : 'authorize'}?${
+      query({ oauth_token: requestToken, force_login: forceLogin, screen_name: screenName })
+      }`);
 
-  const params = new URLSearchParams(result.url.split('?')[1]);
+    const params = new URLSearchParams(result.url.split('?')[1]);
     if (result.type === 'success' && params.has('oauth_verifier')) {
-    //console.log('oauth_verifier: ', params.get('oauth_verifier')); 
       return getAccessToken(
-    {...tokens, requestToken, requestTokenSecret},
-     (
-      usePin ?
-        callbackUrl :
-        params.get('oauth_verifier')
-    )
-  );
-}
+        { ...tokens, requestToken, requestTokenSecret },
+        (
+          usePin ?
+            callbackUrl :
+            params.get('oauth_verifier')
+        )
+      );
     }
+  }
 }
